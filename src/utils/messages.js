3577565cc945002cae7c4909ea24a24e7c74449c
blueskyleader01 <blueskyleader01@hotmail.com>
2023-09-26 17:20:52 +0200

    first commit

---
+++ src/utils/messages.js
@@ -0,0 +1,21 @@
+const generateMessage = (username,text) =>{
+    return {
+        username,
+        text,
+        createdAt : new Date().getTime() 
+    }
+}
+
+const generateLocationMessage = (username,data) =>{
+    return {
+        username,
+        url: `https://www.google.com/maps?q=${data.latitude},${data.longitude}`,
+        createdAt : new Date().getTime() 
+    }
+}
+
+
+module.exports = {
+    generateMessage,
+    generateLocationMessage
+}
